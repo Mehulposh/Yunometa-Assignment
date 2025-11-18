7f31ca1223bf367c1e3034a0984edb3b9da5412f
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-11-18 20:12:36 +0530

    created the required controllers

---
+++ Backend/controllers/TaskController.js
@@ -0,0 +1,84 @@
+//import the services class
+import TaskService from '../services/TaskServices'
+
+//creating the new instance of the services class
+const TaskServiceInstance = new TaskService();
+
+//contoller to fetch all tasks in db
+const getTasks = async (req, res) => {
+  try {
+    //fetching all tasks in db
+    const tasks = await TaskServiceInstance.find();
+
+    //sending the response back with the status
+    res.status(200).json(tasks);
+
+  } catch (err) {
+    //error response
+    res.status(500).json({ error: err.message });
+  }
+};
+
+//controller to create a new task
+const createTask = async (req, res) => {
+  try {
+    //destructuring the title and description from the received request
+    const { title, description} = req.body;
+
+    //creating new task 
+    const newTask = await TaskServiceInstance.create({
+      title,
+      description
+    });
+
+    //sending the response back with the status
+    res.status(201).json(newTask);
+
+  } catch (err) {
+    //error response
+    res.status(500).json({ error: err.message });
+  }
+};
+
+//constroller to update an existing task
+const updateTask = async (req, res) => {
+  try {
+    //destructuring task id from the received params
+    const { id } = req.params;
+
+    //sending the id and req body to the update service to update the task in db
+    const result = await TaskServiceInstance.update(id, req.body);
+
+    //sending the response back with the status
+    res.status(200).json(result);
+
+  } catch (err) {
+    //error response
+    res.status(500).json({ error: err.message });
+  }
+};
+
+//controller to delete an existing task
+const deleteTask = async (req, res) => {
+  try {
+    //destructuring task id from the received params
+    const { id } = req.params;
+
+    //sending the id to the delete service to delete the task from db
+    const result = await TaskServiceInstance.delete(id);
+
+    //sending the response back with the status
+    res.status(204).send(result);
+  } catch (err) {
+    //error response
+    res.status(500).json({ error: err.message });
+  }
+};
+
+//exporting all the controllers
+module.exports = {
+  getTasks,
+  createTask,
+  updateTask,
+  deleteTask,
+};
