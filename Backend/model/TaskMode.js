7bb51c47335ab99824efe39fac920ac9283f5af9
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-11-18 19:10:18 +0530

    created a model for tasks

---
+++ Backend/model/TaskMode.js
@@ -0,0 +1,15 @@
+import mongoose  from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    status: { type: String, enum: ["PENDING", "COMPLETED"], default: "PENDING" },
+    
+  },
+  { timestamps: true }
+);
+
+const taskModel = mongoose.model("Task", taskSchema);
+
+module.exports = taskModel;
