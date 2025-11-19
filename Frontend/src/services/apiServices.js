5930c950ef3d1f32b4faea6bc8f72cc74f2ecc48
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-11-19 15:49:05 +0530

    added comments in api services

--- Frontend/src/services/apiServices.js
+++ Frontend/src/services/apiServices.js
@@ -1,25 +1,33 @@
+//importing axios for calling api
 import axios from "axios";
+//import baseurl from backend folder
 import {backend} from '../apiEndpoint/backendEndpoint'
 
+//storing usr in a variable
 const API_URL = backend.endpoint ;
 
+//fetching the data from the backend
 export const fetchTasks = async (page) => {
   const response = await axios.get(`${API_URL}?page=${page}&limit=5`);
   return response.data;
 };
 
+//sending the new task data to the db
 export const createTask = async (formData) => {
   await axios.post(API_URL, formData);
 };
 
+//updating the tasks in the db
 export const updateTask = async (taskId, taskData) => {
   await axios.patch(`${API_URL}/${taskId}`, taskData);
 };
 
+//deleting the task from the db
 export const deleteTask = async (taskId) => {
   await axios.delete(`${API_URL}/${taskId}`);
 };
 
+//changing the status of the task from pending to completed
 export const markTaskAsDone = async (taskId) => {
   await axios.patch(`${API_URL}/${taskId}`, { status: "COMPLETED"});
 };
