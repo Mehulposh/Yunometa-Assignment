fe3d3df13d0054d0de173f7c73011cb00230a116
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-11-19 15:35:22 +0530

    added header and base url for the backend api

---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-blue-500 text-gray-200 font-semibold text-3xl px-5 underline py-2' >
+        YA
+    </div>
+  )
+}
+
+export default Header
